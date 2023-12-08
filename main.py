21641bf9b7c08544812ba65eee4b4f9fb5be3c23
YuliaCheprasova <cheprasovaja@yandex.ru>
2023-12-08 14:01:46 +0700

    all ready done

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ def Y(x1, x2, outliers_percent=10):
         noise = np.random.normal(0, 0.5, 1)[0]
         noise = np.clip(noise, (-y[i]*percent)/100, (y[i] * percent)/100)
         y[i] += noise
-        if np.random.rand() < outliers_percent / 100:
-            intensity = np.random.uniform(-5.0, 7.0)  # Интенсивность выброса
-            y[i] += intensity
+        #if np.random.rand() < outliers_percent / 100:
+            #intensity = np.random.uniform(-10.0, 15.0)
+            #y[i] += intensity
     return y
 
 
@@ -36,13 +36,6 @@ def weighted_least_squares(params, x1, x2, y, w, kind_of_func):
     return np.sum(residuals ** 2)
 
 
-"""def Y_true(x1, x2):
-    y=np.zeros(len(x1))
-    for i in range(len(x1)):
-        y[i]=m.sin(x1[i]) + x2[i]/10
-    return y"""
-
-
 def H(i, r, x1, x2):
     hmax1, hmax2 = 0, 0
     for j in range(i - r, i + r):
@@ -56,9 +49,17 @@ def H(i, r, x1, x2):
     return hmax1, hmax2
 
 
-def count_W(z):
+def count_W2(z):
     if abs(z)<1:
         W = (1-z**2)**2
+    else:
+        W = 0
+    return W
+
+
+def count_W3(z):
+    if abs(z)<1:
+        W = (1-abs(z)**3)**3
     else:
         W = 0
     return W
@@ -67,91 +68,118 @@ def count_W(z):
 def count_w(x1, x2, i, h1, h2):
     w = np.zeros(len(x1))
     for j in range(len(x1)):
-        w[j] = count_W((x1[j]-x1[i])/h1)*count_W((x2[j]-x2[i])/h2)
+        w[j] = count_W3((x1[j]-x1[i])/h1)*count_W3((x2[j]-x2[i])/h2)
     return w
 
 
 def count_w_robast(E, mead):
     w = np.zeros(len(E))
     for j in range(len(E)):
-        w[j] = count_W(E[j] / (6*mead))
+        w[j] = count_W2(E[j] / (6*mead))
     return w
 
-def Ypredict(start, end, kind_of_func, ypredict, params, x1, x2, E, y):
+
+def Ypredict(start, end, i, kind_of_func, ypredict, params, x1, x2, E, y):
+    ypred = np.zeros(len(x1))
     for j in range(start, end, 1):
         if kind_of_func:
-            ypredict[j] = params[0] + params[1] * x1[j] + params[2] * x2[j]
+            ypred[j] = params[0] + params[1] * x1[j] + params[2] * x2[j]
         else:
-            ypredict[j] = params[0] + params[1] * x1[j] + params[2] * x2[j] + params[3] * x1[j] ** 2 + params[4] * x2[j] ** 2
-        E[j] = (y[j] - ypredict[j]) ** 2
+            ypred[j] = params[0] + params[1] * x1[j] + params[2] * x2[j] + params[3] * x1[j] ** 2 + params[4] * x2[j] ** 2
+        E[j] = (y[j] - ypred[j]) ** 2
+    ypredict[i] = ypred[i]
 
 
 if __name__ == '__main__':
     s = 100
-    kind_of_func = False  # True if linear
+    kind_of_func = True  # True if linear
     if kind_of_func:
         num_param = 3
     else:
         num_param = 5
     m = 2
-    f = 0.1
-    r = int(math.ceil(f * s))
-    print(r)
-    len_test = int(s * 0.2)
-    x_start1 = np.linspace(0, 100, s)
-    x_start2 = np.linspace(0, 100, s)
-    y_start = Y(x_start1, x_start2)
-    x1 = x_start1[:s]
-    x2 = x_start2[:s]
-    x_test1 = x_start1[s:]
-    x_test2 = x_start2[s:]
-    y = y_start[:s]
-    y_test = y_start[s:]
+    x1 = np.linspace(0, 100, s)
+    x2 = np.linspace(0, 200, s)
+    y = Y(x1, x2)
     ypredict = np.zeros(s)
-    ypredict_test = np.zeros(len_test)
     E = np.zeros(s)
     ME = np.zeros(s)
+    points = np.arange(s)
+    """for k in range(1, 10, 1):
+        b=k/10
+        r = int(math.ceil(b * s))
+        step = r
+        print(r)
+        for i in range(0, s):
+            h1, h2 = H(i, r, x1, x2)
+            w = count_w(x1, x2, i, h1, h2)
+            if i-step<0:
+                begin=0
+            else:
+                begin=i-step
+            if i+step>s:
+                end=s
+            else:
+                end=i+step
+            x1_param=x1[begin:end]
+            x2_param = x2[begin:end]
+            y_param = y[begin:end]
+            w_param = w[begin:end]
+            params = optimize.minimize(weighted_least_squares, x0=np.ones(num_param), args=(x1_param, x2_param, y_param, w_param, kind_of_func)).x
+            Ypredict(begin, end, i, kind_of_func, ypredict, params, x1, x2, E, y)
+            for iter in range(m):
+                E_param = E[begin:end]
+                ME_param = ME[begin:end]
+                med = statistics.median(E_param)
+                for j in range(len(E_param)):
+                    ME_param[j] = abs(E_param[j]-med)
+                mead = statistics.median(ME_param)
+                w = count_w_robast(E, mead)
+                w_param = w[begin:end]
+                params = optimize.minimize(weighted_least_squares, x0=np.ones(num_param), args=(x1_param, x2_param, y_param, w_param, kind_of_func)).x
+                Ypredict(begin, end, i, kind_of_func, ypredict, params, x1, x2, E, y)
+        plt.plot(points, ypredict, label=f'b={b}')"""
+
+    b = 0.03
+    r = int(math.ceil(b * s))
+    step = r
+    print(r)
     for i in range(0, s):
         h1, h2 = H(i, r, x1, x2)
         w = count_w(x1, x2, i, h1, h2)
-        params = optimize.minimize(weighted_least_squares, x0=np.ones(num_param), args=(x1, x2, y, w, kind_of_func)).x
-        if kind_of_func:
-            ypredict[i] = params[0] + params[1] * x1[i] + params[2] * x2[i]
+        if i - step < 0:
+            begin = 0
         else:
-            ypredict[i] = params[0] + params[1] * x1[i] + params[2] * x2[i] + params[3] * x1[i] ** 2 + params[4] * x2[i] ** 2
-        E[i] = y[i] - ypredict[i]
-    for iter in range(m):
-        med = statistics.median(E)
-        for i in range(s):
-            ME[i] = abs(E[i]-med)
-        mead = statistics.median(ME)
-        for i in range(s):
+            begin = i - step
+        if i + step > s:
+            end = s
+        else:
+            end = i + step
+        x1_param = x1[begin:end]
+        x2_param = x2[begin:end]
+        y_param = y[begin:end]
+        w_param = w[begin:end]
+        params = optimize.minimize(weighted_least_squares, x0=np.ones(num_param),
+                                   args=(x1_param, x2_param, y_param, w_param, kind_of_func)).x
+        Ypredict(begin, end, i, kind_of_func, ypredict, params, x1, x2, E, y)
+        """for iter in range(m):
+            E_param = E[begin:end]
+            ME_param = ME[begin:end]
+            med = statistics.median(E_param)
+            for j in range(len(E_param)):
+                ME_param[j] = abs(E_param[j] - med)
+            mead = statistics.median(ME_param)
             w = count_w_robast(E, mead)
-            params = optimize.minimize(weighted_least_squares, x0=np.ones(num_param), args=(x1, x2, y, w, kind_of_func)).x
-            if kind_of_func:
-                ypredict[i] = params[0] + params[1] * x1[i] + params[2] * x2[i]
-            else:
-                ypredict[i] = params[0] + params[1] * x1[i] + params[2] * x2[i] + params[3] * x1[i] ** 2 + params[4] * x2[i] ** 2
-            E[i] = y[i] - ypredict[i]
+            w_param = w[begin:end]
+            params = optimize.minimize(weighted_least_squares, x0=np.ones(num_param),
+                                       args=(x1_param, x2_param, y_param, w_param, kind_of_func)).x
+            Ypredict(begin, end, i, kind_of_func, ypredict, params, x1, x2, E, y)"""
+    plt.plot(points, ypredict, label=f'b={b}')
 
-    points = np.arange(s)
+
     plt.plot(points, y, label='Истинные значения')
-    plt.plot(points, ypredict, label='Сглаженные значения')
     plt.legend()
     plt.show()
-    #MSE = sum(E) / s
-    #print(f'MSE = {MSE}')
 
 
 
-
-# как тут делать тест, если парметры рассчитываются для каждой отдельной точки
-# взять не каждую следующую точку, а интервалами
-# Примените LOWESS
-#lowess = sm.nonparametric.lowess(y, x, frac=0.3)  # frac - параметр, определяющий ширину окна
-
-# Результат LOWESS - это массив точек (x, y)
-"""smoothed_x, smoothed_y = lowess.T
-plt.scatter(x,y)
-plt.plot(smoothed_x, smoothed_y)
-plt.show()"""
